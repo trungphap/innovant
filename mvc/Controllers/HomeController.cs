771f6546001b37e70aaec6afcabd09f3e979322d
bfa1trung <bfa1trung@yahoo.com>
2023-12-16 23:08:43 +0100

    displayimages

---
+++ mvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Attr1 = "/img/attr1.png";
+        ViewBag.Attr2 = "/img/attr2.png";
+        ViewBag.Attr5 = "/img/attr5.png";
+        ViewBag.Attr6 = "/img/attr6.png";
+        ViewBag.Correlation = "/img/correlation.png";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
